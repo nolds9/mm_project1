7c998b1d083e49e89f19603785cbf56d78dbf337
Melissa Miller <lis.d.miller@gmail.com>
2016-02-22 12:15:40 -0500

    Creates mutiple cards

--- script.js
+++ script.js
@@ -6,28 +6,31 @@
 
 var game = {
   board: document.querySelector('.board'),
-  card: document.createElement('div'),
-  face: document.createElement('p'),
-  value: document.createTextNode('1'),
+  deck1: [],
 
-  createCard: function() {
-    this.face.appendChild(this.value); // assigns a value to the face
-    this.face.classList.add('face'); // provides style to the face of the card
-
-    this.card.classList.add('card'); // creates card
-
-    this.card.appendChild(this.face);
-
-    this.card.classList.add('back'); // hides face value of card
-
-    this.board.appendChild(this.card);
-
-    this.card.addEventListener('click', this.flipCard);
+  createCard: function(faceValue) {
+    var value = document.createTextNode(faceValue);
+    card = document.createElement('div');
+    face = document.createElement('p');
+    face.appendChild(value); // assigns a value to the face
+    face.classList.add('face'); // provides style to the face of the card
+    card.classList.add('card'); // creates card
+    card.appendChild(face);
+    card.classList.add('back'); // hides face value of card
+    this.board.appendChild(card);
+    card.addEventListener('click', this.flipCard);
   },
 
   flipCard: function() {
     this.classList.toggle('back');
+  },
+
+  createCards: function() {
+      for(var i = 0; i < 10; i++) {
+        this.createCard(1);
+      }
+
   }
 };
 
-game.createCard();
+game.createCards();
