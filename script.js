79533d35240fe723ed574c5bbf2012d17df0d598
Melissa Miller <lis.d.miller@gmail.com>
2016-02-22 10:30:23 -0500

    Creates 1 basic card

---
+++ script.js
@@ -0,0 +1,12 @@
+//display 1 card (back)
+//display 1 card front
+// flip that card back and forth
+//make a deck of cards (can be numbered 1-n for now)
+// display all cards
+
+var card = document.createElement('div');
+card.classList.add('card');
+
+var board = document.querySelector('.board');
+
+board.appendChild(card);
