11126d068f12256e97a06fe54efcbe685f54b92b
Melissa Miller <lis.d.miller@gmail.com>
2016-02-22 11:07:44 -0500

    Adds a back and front to the card

--- script.js
+++ script.js
@@ -5,8 +5,24 @@
 // display all cards
 
 var card = document.createElement('div');
-card.classList.add('card');
+var face = document.createElement('p');
+var value = document.createTextNode('1');
+
+face.appendChild(value); // assigns a value to the face
+face.classList.add('face'); // provides style to the face of the card
+
+card.classList.add('card'); // creates card
+
+card.appendChild(face);
+
+card.classList.add('back'); // hides face value of card
 
 var board = document.querySelector('.board');
 
 board.appendChild(card);
+
+card.addEventListener('click', flipCard);
+
+function flipCard() {
+  console.log("you clicked the card!");
+}
