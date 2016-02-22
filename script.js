e554471fda61b15bccc7b2c927a96f93fa8d6aa6
Melissa Miller <lis.d.miller@gmail.com>
2016-02-22 16:55:38 -0500

    Creates a data attribute to identify location of cards when clicked

--- script.js
+++ script.js
@@ -3,16 +3,18 @@
 // flip that card back and forth
 //make a deck of cards (can be numbered 1-n for now)
 // display all cards
-//
 
 var game = {
   board: document.querySelector('.board'),
   deck: [],
+  card1: null,
+  card2: null,
 
 
-  createCard: function(faceValue) {
+  createCard: function(faceValue, index) {
     var value = document.createTextNode(faceValue);
     card = document.createElement('div');
+    card.setAttribute('data-deckPosition', index);
     face = document.createElement('p');
     face.appendChild(value); // assigns a value to the face
     face.classList.add('face'); // provides style to the face of the card
@@ -25,14 +27,14 @@ var game = {
 
   flipCard: function() {
     this.classList.toggle('back');
+    game.playGame(this.getAttribute('data-deckPosition'));
   },
 
   createCards: function() {
     this.buildDeck();
       for(var i = 0; i < this.deck.length; i++) {
-        this.createCard(this.deck[i]);
+        this.createCard(this.deck[i], i);
       }
-
   },
 
   buildDeck: function() {
@@ -58,8 +60,44 @@ var game = {
     }
   },
 
-  playRound: function() {
-    
+  playGame: function(position) {
+    var currentCard = document.getElementsByClassName('card');
+    var value = currentCard[position].textContent;
+    console.log(value);
+
+
+    // if(!isBack && this.card1 === null) {
+    //   this.card1 = number;
+    // }
+    // else if(!isBack && this.card2 === null) {
+    //   this.card2 = number;
+    //   this.matchNumbers();
+    // }
+    // else {
+    //   console.log('play game else path');
+    // }
+
+    // check if number is visible
+      // if not ?
+      // if yes, is this the first number?
+        // if yes, get a second number
+        // if no, compare numbers
+
+        // do numbers match?
+          // if yes, disable clicking on them
+          // if no, flip back over
+  },
+
+  matchNumbers: function() {
+    if(this.card1 == this.card2){
+      console.log('hooray');
+
+    }
+    else {
+      console.log("booo!");
+    }
+    this.card1 = null;
+    this.card2 = null;
   }
 };
 
